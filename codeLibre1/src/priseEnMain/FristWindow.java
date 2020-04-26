683f382d26bc44c59f81a9b6bdc617c059da1c78
Gess <Gess@LAPTOP-EU5M26AM>
2020-04-25 21:15:00 -0700

    tools

---
+++ codeLibre1/src/priseEnMain/FristWindow.java
@@ -0,0 +1,102 @@
+package priseEnMain;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FristWindow extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JLabel lblNewLabel;
+	private JButton button;
+	private JButton btnNewButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FristWindow frame = new FristWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FristWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		contentPane.add(getTextField(), BorderLayout.NORTH);
+		contentPane.add(getLblNewLabel(), BorderLayout.EAST);
+		contentPane.add(getButton(), BorderLayout.WEST);
+		contentPane.add(getBtnNewButton(), BorderLayout.SOUTH);
+	}
+
+	private JTextField getTextField() {
+		if (textField == null) {
+			textField = new JTextField();
+			textField.setColumns(10);
+		}
+		return textField;
+	}
+	private JLabel getLblNewLabel() {
+		if (lblNewLabel == null) {
+			lblNewLabel = new JLabel("Chrisnelle McCanna");
+			lblNewLabel.addComponentListener(new ComponentAdapter() {
+				@Override
+				public void componentMoved(ComponentEvent arg0) {
+				}
+			});
+			lblNewLabel.setIcon(new ImageIcon(FristWindow.class.getResource("/priseEnMain/unnamed.jpg")));
+			lblNewLabel.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
+		}
+		return lblNewLabel;
+	}
+	private JButton getButton() {
+		if (button == null) {
+			button = new JButton("New button");
+		}
+		return button;
+	}
+	private JButton getBtnNewButton() {
+		if (btnNewButton == null) {
+			btnNewButton = new JButton("OK");
+			btnNewButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					JOptionPane.showMessageDialog(null, "Bienvenue" + textField.getText());
+				}
+			});
+		}
+		return btnNewButton;
+	}
+}
